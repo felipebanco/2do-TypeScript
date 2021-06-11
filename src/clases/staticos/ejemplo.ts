799d1c7b796f4923b55ff7a9e7b49e650147f403
Mauricio Salafia <mauricio.salafia@kaplan.edu>
2021-06-11 19:53:45 -0300

    Agrego ejemplo de atributo y metodos estaticos

---
+++ src/clases/staticos/ejemplo.ts
@@ -0,0 +1,36 @@
+
+class Circulo {
+    static readonly PI = 3.14;
+    nombre:string;
+
+    constructor(n: string) {
+        this.nombre = n;
+    }
+
+    static calcularArea(radio: number): number {
+        // console.info(this.nombre); en un método estatico no puedo acceder a valores no estaticos de la clase
+        return this.PI * Math.pow(radio, 2);
+    }
+
+}
+
+class ClaseUno {
+    ejecutar() {
+        console.info(Circulo.PI);
+        // Circulo.PI = 33333; no permite porque el atributo es readonly
+    }
+}
+
+const c1 = new ClaseUno();
+c1.ejecutar();
+
+class ClaseDos {
+    ejecutar() {
+        console.info(Circulo.PI);
+    }
+}
+
+const c2 = new ClaseDos();
+c2.ejecutar();
+
+console.info("El area del circulo de radio 10 es: " + Circulo.calcularArea(10));
